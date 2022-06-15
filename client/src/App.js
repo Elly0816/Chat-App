a276983584ce2720faca2e6f0f4e1ca13cef2559
Eleazar Udo <elzoremmanuel@gmail.com>
2022-06-15 21:17:37 +0300

    Added some styling to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Messages from './components/Messages';
 import  io from 'socket.io-client';
 import Entry from './components/Entry';
@@ -21,11 +21,16 @@ function App() {
     setMessages([...messages, arg])
   })
 
+
+
   return (
-    <div className='App'>
+    <div className='app'>
       <Header/>
-      {messages.map((message, index) => 
-         <Messages key={message+index} message={message}/>)}
+      <div className='message-container' ref='messages'>
+        {messages.map((message, index) => 
+          <Messages key={message+index} message={message}/>)}
+      </div>
+      
       <Entry sendMessage={sendMessage}/>
     </div>
   );
--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -2,18 +2,9 @@ import React from 'react';
 
 
 export default function Messages(props){
-    // const [ response, setResponse ] = useState();
-    
-    
-    // useEffect(() => {
-    // const socket = io.connect('http://localhost:5000/');
-    // socket.on('FromAPI', (data) => {
-    //     setResponse(data);
-    // });
-    // }, []);
 
 
-    return <div>
+    return <div className='messages'>
         <h6>{ props.message }</h6>
     </div>
 }
