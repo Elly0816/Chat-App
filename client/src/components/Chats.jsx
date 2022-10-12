a47c5219e9d4b508274c43cac960f66176e7e811
Eleazar Udo <elzoremmanuel@gmail.com>
2022-10-12 10:31:12 +0300

    chats variable is conditional and length property is applied when the chats variable is available

--- client/src/components/Chats.jsx
+++ client/src/components/Chats.jsx
@@ -35,7 +35,7 @@ export default function Chats(props){
    <div className='chatshead'>
    <h3>Chats</h3>
    {/* <h6>{chats?.length}</h6>{chats?.length > 1? <h6>chats</h6> : <h6>chat</h6>} */}
-   {chats?.length > 1 ? <h6>{chats.length} chats</h6> : <h6>{chats.length} chat</h6>}
+   {chats?.length > 1 ? <h6>{chats?.length} chats</h6> : <h6>{chats?.length} chat</h6>}
    </div>
    <div>
       <Form>
